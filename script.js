6b7a01ed944d6832b17341cca1b31a6b7995616b
omaralashqa <OY8773669@gmail.com>
2024-12-12 16:26:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgslider(x) {
+    document.querySelector('.app').src = x;
+}
+function changeCircleColor(color){
+    const home=document.querySelector('.home');
+    home.style.background =color;
+}
